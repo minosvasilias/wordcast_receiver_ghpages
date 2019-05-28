3cb579f75b652ba31ca882ca0d4dd7be0e0ec997
minosvasilias <minosvasilias@gmail.com>
2019-05-28 20:24:39 +0200

    Updates

---
+++ precache-manifest.d1aa78f1255e8a1aea43b463750c657c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "741233a65bee6a6593227714645d3169",
+    "url": "/wordcast_receiver_ghpages/index.html"
+  },
+  {
+    "revision": "f4e3cd93b9426f5aa32c",
+    "url": "/wordcast_receiver_ghpages/static/css/main.8b42e3fd.chunk.css"
+  },
+  {
+    "revision": "601af9ac0de845aa6903",
+    "url": "/wordcast_receiver_ghpages/static/js/2.baa90864.chunk.js"
+  },
+  {
+    "revision": "f4e3cd93b9426f5aa32c",
+    "url": "/wordcast_receiver_ghpages/static/js/main.ed3c4c2d.chunk.js"
+  },
+  {
+    "revision": "ca247b8d9545193dea07",
+    "url": "/wordcast_receiver_ghpages/static/js/runtime~main.0569e602.js"
+  },
+  {
+    "revision": "c4dcc4ed917eba76e825075d468d4cf3",
+    "url": "/wordcast_receiver_ghpages/static/media/wordcastlogo_white.c4dcc4ed.svg"
+  }
+]);
