5305950a85454b24307863e25dfbd96291c4d370
minosvasilias <minosvasilias@gmail.com>
2019-06-06 18:13:12 +0200

    Updates

---
+++ precache-manifest.bd656ccacf2dd93a17211463727126d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d80c588bb5a34be5419f07fc1b8f248",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2024855a6577515f4dcf",
+    "url": "/static/css/main.8caa4fbf.chunk.css"
+  },
+  {
+    "revision": "6f0a50f0584eda228afb",
+    "url": "/static/js/2.7d8e3443.chunk.js"
+  },
+  {
+    "revision": "2024855a6577515f4dcf",
+    "url": "/static/js/main.de465c6d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c4dcc4ed917eba76e825075d468d4cf3",
+    "url": "/static/media/wordcastlogo_white.c4dcc4ed.svg"
+  }
+]);
